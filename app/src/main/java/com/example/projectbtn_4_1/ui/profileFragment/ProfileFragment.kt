8dd9f75b50e97162bcbe368e03c7ea700fd4e52e
musabekov-1 <bestmusabekov@gmail.com>
2023-12-16 16:10:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/projectbtn_4_1/ui/profileFragment/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package com.example.projectbtn_4_1.ui.profileFragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projectbtn_4_1.databinding.FragmentHomeBinding
+import com.example.projectbtn_4_1.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
